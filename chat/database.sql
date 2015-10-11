840c7bb89a8bcea7c8c5788a16ee4bf5386fd0af
Guilherme Salomão <gsalomao_freela@hotmail.com>
2015-10-11 02:46:23 -0300

    SQL database specifications

---
+++ chat/database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `messenger`.`messages` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user` VARCHAR(45) NOT NULL,
+  `message` TEXT NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));CREATE TABLE `messenger`.`messages` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user` VARCHAR(45) NOT NULL,
+  `message` TEXT NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
