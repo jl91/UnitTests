f52d9f2d12dd05d436a87e3cf0308c0b1f6e06af
john lennon de melo fernandes <johnlennon@progiro.com.br>
2015-10-11 01:24:25 -0300

    install angular, jquery, bootstrap and make tests dir

--- chat/public/index.php
+++ chat/public/index.php
@@ -1,5 +1,21 @@
-<?php 
+<!DOCTYPE html>
 
+<html ng-app="chat">
+    <head>
+        <title>Chat</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    </head>
+    <body>
+        <input ng-model="text">
+        <div ng-bind="text">
 
+        </div>
+        <script type="text/javascript"  src="https://code.jquery.com/jquery-2.1.4.min.js" ></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.29/angular.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="app/init.js"></script>
 
-echo "hello world";
+    </body>
+</html>
